e8cc33525e7adbd8cec6e1c9481d1afeae8ca192
Theo Beers <tb3367@drexel.edu>
2024-12-22 09:08:25 -0500

    Move secrets to env

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 
 set -Eeuo pipefail
 
+source .env
+
+envsubst <config.yml >config_rendered.yml
+
 helm upgrade --cleanup-on-fail \
 	--install engr-131-jhub jupyterhub/jupyterhub \
 	--namespace jhub \
 	--create-namespace \
 	--version=3.3.8 \
 	--timeout=10m \
-	--values config.yml
+	--values config_rendered.yml
