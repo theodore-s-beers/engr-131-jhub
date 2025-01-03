b07d46eff60d70925d79240aa969a6bd5f98dafc
jagar2 <jca92@drexel.edu>
2025-01-03 17:04:20 -0500

    add exams build

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,13 @@ helm upgrade --cleanup-on-fail \
 	--version=3.3.8 \
 	--timeout=10m \
 	--values config_rendered.yml
+
+envsubst <config_exams.yml >config_exams_rendered.yml
+
+helm upgrade --cleanup-on-fail \
+	--install engr-131-jhub jupyterhub/jupyterhub \
+	--namespace jhub \
+	--create-namespace \
+	--version=3.3.8 \
+	--timeout=10m \
+	--values config_exams_rendered.yml
