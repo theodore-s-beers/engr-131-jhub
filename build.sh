d584a789a53da007d36eeb7189be9de838f1f664
Theo Beers <tb3367@drexel.edu>
2025-01-06 12:09:45 -0500

    Reorder args

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ docker buildx build \
 	--platform linux/amd64,linux/arm64 \
 	-t katomyomachia/engr-131-jhub:latest \
 	-f Dockerfile \
-	. --push
+	--push .
 
 docker buildx build \
 	--platform linux/amd64,linux/arm64 \
 	-t katomyomachia/engr-131-jhub-exams:latest \
 	-f exams.Dockerfile \
-	. --push
+	--push .
